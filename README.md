2f5f0de85495da59c284a62dceac139e50905081
baldyeagle <austin.taylor.baldwin@gmail.com>
2020-05-07 11:59:46 -0700

    Reformat function and example usage

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
 # faa-aircraft-registry
-This Python package formats the multiple CSV files provided from the [FAA Aircraft Registry](https://www.faa.gov/licenses_certificates/aircraft_certification/aircraft_registry/releasable_aircraft_download/) and reformats them into a single list of Python dictionaries allowing for easier use for additional programmatic usage.
+This Python package formats the multiple CSV files provided from the [FAA Aircraft Registry](https://registry.faa.gov/aircraftinquiry/) and reformats them into a single list of Python dictionaries allowing for easier use for programmatic usage.
+
+## Data Information
+The downloaded data is read from multiple comma-delimited files contained within one zipped file. The expected structure is:
+- `ReleasableAircraft.zip`
+  - `ACFTREF.txt`: aircraft reference file by make/model/series sequence
+  - `ardata.pdf`: documentation for file content and configuration
+  - `DEALER.txt`: aircraft dealer applicant file
+  - `DEREG.txt`: deregistered aircraft file
+  - `DOCINDEX.txt`: aircraft document index file
+  - `ENGINE.txt`: engine reference file
+  - `MASTER.txt`: aircraft registration master file
 
 ## Example Usage
 ```python
+import io
+import zipfile
+import requests
 from read_registry import read
 
 url = 'http://registry.faa.gov/database/ReleasableAircraft.zip'
-
-registrations = read(url)
+r = requests.get(url, stream=True)
+if r.ok:
+    with zipfile.ZipFile(io.BytesIO(r.content)) as z:
+        registrations = read(z)
 ```
--- read_registry.py
+++ read_registry.py
@@ -1,5 +1,3 @@
-import requests
-import zipfile
 import io
 from aircraft import read as read_aircraft
 from engines import read as read_engines
@@ -7,51 +5,32 @@ from master import read as read_master
 
 url = 'http://registry.faa.gov/database/ReleasableAircraft.zip'
 
+# r = requests.get(url, stream=True)
+# if r.ok:
+#     with zipfile.ZipFile(io.BytesIO(r.content)) as z:
+#         registrations = read(z)
 
-def read(url):
+
+def read(zipped_file):
     """
     Documentation
     """
 
-    registrations = []
+    registrations = {}
 
-    r = requests.get(url, stream=True)
-    if r.ok:
-        with zipfile.ZipFile(io.BytesIO(r.content)) as z:
+    # Read ACFTREF.txt csv file
+    with zipped_file.open('ACFTREF.txt', 'r') as f:
+        csvfile = io.TextIOWrapper(f, 'utf-8-sig')
+        registrations['aircraft'] = read_aircraft(csvfile)
 
-            # Read ACFTREF.txt csv file
-            with z.open('ACFTREF.txt', 'r') as f:
-                csvfile = io.TextIOWrapper(f, 'utf-8-sig')
-                aircraft_dict = read_aircraft(csvfile)
+    # Read ENGINE.txt csv file
+    with zipped_file.open('ENGINE.txt', 'r') as f:
+        csvfile = io.TextIOWrapper(f, 'utf-8-sig')
+        registrations['engines'] = read_engines(csvfile)
 
-            # Read DEALER.txt csv file
-            # with z.open('DEALER.txt', 'r') as f:
-            #     dealer_df = pd.read_csv(f, dtype='object')
+    # Read MASTER.txt csv file
+    with zipped_file.open('MASTER.txt', 'r') as f:
+        csvfile = io.TextIOWrapper(f, 'utf-8-sig')
+        registrations['master'] = read_master(csvfile)
 
-            # Read DEREG.txt csv file
-            # with z.open('DEREG.txt', 'r') as f:
-            #     dereg_df = pd.read_csv(f, dtype='object')
-
-            # Read DOCINDEX.txt csv file
-            # with z.open('DOCINDEX.txt', 'r') as f:
-            #     docindex_df = pd.read_csv(f, dtype='object')
-
-            # Read ENGINE.txt csv file
-            with z.open('ENGINE.txt', 'r') as f:
-                csvfile = io.TextIOWrapper(f, 'utf-8-sig')
-                engine_dict = read_engines(csvfile)
-
-            # Read MASTER.txt csv file
-            with z.open('MASTER.txt', 'r') as f:
-                csvfile = io.TextIOWrapper(f, 'utf-8-sig')
-                master_dict = read_master(csvfile)
-
-            # Read RESERVED.txt csv file
-            # with z.open('RESERVED.txt', 'r') as f:
-            #     reserved_df = pd.read_csv(f, dtype='object')
-
-        return {
-            'aircraft': aircraft_dict,
-            'engines': engine_dict,
-            'master': master_dict,
-        }
+    return registrations
