58e9fb4241400f0e9abd2deccc3933f89b556f37
baldyeagle <austin.taylor.baldwin@gmail.com>
2020-05-08 10:22:09 -0700

    Join aircraft and engines for registrations.

--- read_registry.py
+++ read_registry.py
@@ -26,4 +26,21 @@ def read(zipped_file):
         csvfile = io.TextIOWrapper(f, 'utf-8-sig')
         registrations['master'] = read_master(csvfile)
 
+    for pk, registration in registrations['master'].items():
+        # Find engine from engine code in master list
+        engine_code = registration.get('engine_manufacturer_code', None)
+        if engine_code and engine_code in registrations['engines']:
+            registration['engine_details'] = registrations['engines'].get(
+                engine_code)
+        else:
+            registration['engine_details'] = {}
+
+        # Find airframe from airframe code in master list
+        aircraft_code = registration.get('aircraft_manufacturer_code', None)
+        if aircraft_code and aircraft_code in registrations['aircraft']:
+            registration['aircraft_details'] = registrations['aircraft'].get(
+                aircraft_code)
+        else:
+            registration['aircraft_details'] = {}
+
     return registrations
